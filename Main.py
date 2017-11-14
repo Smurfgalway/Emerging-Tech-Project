390391e1f72430d6e75ee43647cf7f63e1a1de9f
smurfgalway <g00323639@gmit.ie>
2017-11-14 11:36:49 +0000

    python file using flask framework created

---
+++ Main.py
@@ -0,0 +1,14 @@
+#Main Python page using flask framework
+
+import flask as fl
+from flask import render_template, g, request# imports the render template property which allows html pages be loaded as the template for the pages on the server
+
+app = fl.Flask(__name__)
+
+@app.route("/")
+def name():
+    return render_template('Index.html')
+
+
+if __name__ == "__main__":
+    app.run() # runs the app
