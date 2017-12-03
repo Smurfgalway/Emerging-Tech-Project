7c9ca6eba23cf11a0f05ba7e9a509d003f7c31c0
smurfgalway <g00323639@gmit.ie>
2017-12-03 22:04:47 +0000

    changes made to main.py and render template

--- Main.py
+++ Main.py
@@ -2,13 +2,17 @@
 
 import flask as fl
 from flask import render_template, request# imports the render template property which allows html pages be loaded as the template for the pages on the server
-
+from werkzeug.utils import secure_filename
 app = fl.Flask(__name__)
 
 @app.route("/")
 def name():
     return render_template('Main.html')
 
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 if __name__ == "__main__":
     app.run() # runs the app
